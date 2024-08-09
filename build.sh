ee5381c8bdd338800d91208ff7e878a9923c71b6
Ulrich Peter Berg <peter.berg@rumrussen.de>
2024-08-09 17:31:07 +0200

    add redshift

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ curl -Lo /etc/yum.repos.d/atim-starship-fedora-"${RELEASE}".repo https://copr.fe
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install tmux kcm_systemd plymouth-kcm starship solaar solaar-udev neovim libburn cdrskin xorriso ifuse usbmuxd host-spawn kleopatra
+rpm-ostree install tmux kcm_systemd plymouth-kcm starship solaar solaar-udev neovim libburn cdrskin xorriso ifuse usbmuxd host-spawn kleopatra redshift
 
 # this would install a package from rpmfusion
 rpm-ostree install vlc
